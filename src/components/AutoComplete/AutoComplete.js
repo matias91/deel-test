28a2e7b48d4b70584fa4340856ffa457bd81e8d5
Matias <matias.marcelo.calvo@gmail.com>
2021-05-20 11:51:35 -0300

    Text highlighting

--- src/components/AutoComplete/AutoComplete.js
+++ src/components/AutoComplete/AutoComplete.js
@@ -7,6 +7,7 @@ export default class AutoComplete extends React.Component {
 
     this.state = {
       data: [],
+      inputValue: '',
       predictions: []
     };
   }
@@ -22,27 +23,50 @@ export default class AutoComplete extends React.Component {
   }
 
   filterData = async (evt) => {
+    evt.preventDefault();
     const { data } = this.state;
     const value = evt.target.value;
     const predictions = !!value ? await data.filter(prediction => prediction.name.includes(value)) : [];
 
-    this.setState({ predictions });
+    this.setState({ inputValue: value, predictions });
+  }
+
+  renderItem = (name, index) => {
+    const { inputValue } = this.state;
+    const val =
+      name.slice(0, name.indexOf(inputValue)) +
+      "<b>" + inputValue + "</b>" +
+      name.slice(
+        name.indexOf(inputValue) + inputValue.length,
+        name.length
+      );
+
+    return (
+      <li className="autocomplete__prediction" key={`${name}-${index}`}>
+        <span dangerouslySetInnerHTML={{ __html: val }} />
+      </li>
+    );
   }
 
   renderList = () => {
     return (
       <ul className="autocomplete__predictions">
-        {this.state.predictions.map(({ name }, index) => <li key={`${name}-${index}`}>{name}</li>)}
+        {this.state.predictions.map(({ name }, index) => this.renderItem(name, index))}
       </ul>
     )
   }
 
   render() {
-    const list = !!this.state.predictions.length && this.renderList();
+    const { inputValue, predictions } = this.state;
+    const list = !!predictions.length && this.renderList();
 
     return (
       <section className="autocomplete">
-        <input className="autocomplete__input" onChange={this.filterData} />
+        <input
+          className="autocomplete__input"
+          onChange={this.filterData}
+          value={inputValue}
+        />
 
         {list}
       </section>
